c3f133328c0d1ab4c9e0411f8f0f4863ff1378a4
Cameron Brill <c00brill@gmail.com>
2020-04-09 22:30:59 -0500

    Fixed env var name

--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -3,7 +3,7 @@ import { navigate } from 'gatsby'
 import Recaptcha from 'react-google-recaptcha'
 import Layout from '../layout'
 
-const RECAPTCHA_KEY = process.env.GATSBY_APP_SITE_RECAPTCHA_KEY
+const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY
 if (typeof RECAPTCHA_KEY === 'undefined') {
   throw new Error(`
   Env var GATSBY_APP_SITE_RECAPTCHA_KEY is undefined! 
