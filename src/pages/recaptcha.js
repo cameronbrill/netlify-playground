a6dc484b5ce5d457cb726ccf8893453c3efcda4e
Cameron Brill <c00brill@gmail.com>
2020-04-10 13:16:53 -0500

    Testing with class component.

--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -1,88 +1,87 @@
-import React from 'react'
-import { navigate } from 'gatsby'
-import Layout from '../layout'
+import React from "react";
+import { navigateTo } from "gatsby-link";
 import Recaptcha from "react-google-recaptcha";
 
 const RECAPTCHA_KEY = process.env.SITE_RECAPTCHA_KEY;
 
-
 function encode(data) {
   return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
 }
 
-export default function Contact() {
-  const [state, setState] = React.useState({})
-  const recaptchaRef = React.createRef()
-
-  const handleChange = (e) => {
-    setState({ [e.target.name]: e.target.value })
+export default class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
 
-  const handleRecaptcha = value => {
-    this.setState({ "g-recaptcha-response": value});
-  }
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+  handleRecaptcha = value => {
+    this.setState({ "g-recaptcha-response": value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
+        "form-name": form.getAttribute("name"),
+        ...this.state
+      })
     })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
+      .then(() => navigateTo(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
 
-  return (
-    <Layout>
-      <h1>reCAPTCHA 2</h1>
-      <form
-        name="contact-recaptcha"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-recaptcha="true"
-        onSubmit={handleSubmit}
-      >
-        <noscript>
-          <p>This form won’t work with Javascript disabled</p>
-        </noscript>
-        <p>
-          <label>
-            Your name:
-            <br />
-            <input type="text" name="name" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your email:
-            <br />
-            <input type="email" name="email" onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:
-            <br />
-            <textarea name="message" onChange={handleChange} />
-          </label>
-        </p>
-        <Recaptcha 
-          ref={recaptchaRef}
-          sitekey={RECAPTCHA_KEY}
-          onChange={handleRecaptcha}
-        />
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-    </Layout>
-  )
+  render() {
+    return (
+      <div>
+        <h1>reCAPTCHA 2</h1>
+        <form
+          name="contact-recaptcha"
+          method="post"
+          action="/thanks/"
+          data-netlify="true"
+          data-netlify-recaptcha="true"
+          onSubmit={this.handleSubmit}
+        >
+          <noscript>
+            <p>This form won’t work with Javascript disabled</p>
+          </noscript>
+          <p>
+            <label>
+              Your name:<br />
+              <input type="text" name="name" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your email:<br />
+              <input type="email" name="email" onChange={this.handleChange} />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message:<br />
+              <textarea name="message" onChange={this.handleChange} />
+            </label>
+          </p>
+          <Recaptcha
+            ref="recaptcha"
+            sitekey={RECAPTCHA_KEY}
+            onChange={this.handleRecaptcha}
+          />
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
+      </div>
+    );
+  }
 }
